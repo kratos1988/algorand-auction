b0ca2b5e3d4ae617b19023ebd5173b5ed7b5e390
LuigiC <corno.luigi.89@gmail.com>
2021-04-17 17:26:03 +0200

    fix(frontend): add @angular/material and router

--- algorand-auction-frontend/src/app/app.module.ts
+++ algorand-auction-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
