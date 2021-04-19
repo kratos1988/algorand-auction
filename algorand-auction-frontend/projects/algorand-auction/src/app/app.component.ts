75ab6dceb046544c0b50bdb357c1628503060da8
LuigiC <corno.luigi.89@gmail.com>
2021-04-19 17:47:51 +0200

    fix(frontend): scaffolding, tslint and prettierc config

---
+++ algorand-auction-frontend/projects/algorand-auction/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'algo-auction-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'algorand-auction';
+}
---
+++ algorand-auction-frontend/projects/algorand-auction/src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule
+  ]
+})
+export class CoreModule { }
