7ae6d415f8855db4545c6863cefc54aabe05af68
mmandatori <manuel.mandatori@lastminute.com>
2021-04-27 11:10:54 +0200

    Added test-data fro local config and fix problem build (the angular app was not imported when building)

---
+++ algorand-auction-backend/src/main/resources/sql/test-data.sql
@@ -0,0 +1,8 @@
+SET MODE PostgreSQL;
+insert into AUCTIONS (ID, ITEM_NAME, DESCRIPTION, TITLE, INITIAL_VALUE, STATUS, EXPIRATION_DATE,IMAGE_URL)
+VALUES
+(1,"Mona Lisa's painting",'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua','Mona Lisa',1936.27,'SOLD','2021-04-12 07:20','https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fd%2Fd5%2FMona_Lisa_%2528copy%252C_Hermitage%2529.jpg&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AMona_Lisa_(copy%2C_Hermitage).jpg&tbnid=SzWFLg2Qw8oBQM&vet=12ahUKEwjb0LXZhJ7wAhUBkqQKHUexA10QMygBegUIARDBAQ..i&docid=-GBjMS6AwTJ14M&w=1433&h=1920&q=mona%20lisa%20painting%20free%20license&ved=2ahUKEwjb0LXZhJ7wAhUBkqQKHUexA10QMygBegUIARDBAQ');
+
+insert into BID (ID, AUCTION_ID, AMOUNT, STATUS, USER_ID)
+VALUES
+(1,1,1937.27, 'ACCEPTED', 'manuel_m');
