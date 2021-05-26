e2ef5f0d530edd050a1fe6f4f61d65bf99c05886
mmandatori <manuel.mandatori@lastminute.com>
2021-05-26 16:14:06 +0200

    Changed algorand config

--- algorand-auction-backend/src/main/java/com/algorand/auction/configuration/BlockChainConfiguration.java
+++ algorand-auction-backend/src/main/java/com/algorand/auction/configuration/BlockChainConfiguration.java
@@ -13,10 +13,8 @@ import com.algorand.auction.usecase.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:algorand.properties")
 public class BlockChainConfiguration {
 
     @Value("${purestake.main.url}")
